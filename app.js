70da33a8fbb68ab05f2b45f82301b60a3bf52ba9
FlyingCelingFan <57369474+FlyingCelingFan@users.noreply.github.com>
2020-05-01 03:45:10 -1000

    added darkmode option on click

---
+++ app.js
@@ -0,0 +1,18 @@
+// function teleport() {
+  
+
+    
+// }
+
+// document.getElementById("calculate").onclick = function() {
+//   teleport();
+// };
+
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+document.getElementById("calculate").onclick = function() {
+  teleport();
+};
